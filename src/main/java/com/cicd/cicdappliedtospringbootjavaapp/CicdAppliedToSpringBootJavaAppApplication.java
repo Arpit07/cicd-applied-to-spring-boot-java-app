fe3b631035bfc81254f872b44979c288b28f05db
Arpit07 <arpit.bly@gmail.com>
2019-11-14 12:27:24 +0530

    invalid jar fix

--- src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplication.java
@@ -2,20 +2,19 @@ package com.cicd.cicdappliedtospringbootjavaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.cicd.cicdappliedtospringbootjavaapp.controller.HelloController;
 
 @SpringBootApplication
-@RestController
+//@RestController
 public class CicdAppliedToSpringBootJavaAppApplication {
 	
-	@GetMapping("/")
-	public String home() {
-		return "Hello World from DZONE";
-	}
+	/*
+	 * @GetMapping("/") public String home() { return "Hello World from DZONE"; }
+	 */
 
 	public static void main(String[] args) {
-		SpringApplication.run(CicdAppliedToSpringBootJavaAppApplication.class, args);
+		SpringApplication.run(HelloController.class, args);
 	}
 
 }
